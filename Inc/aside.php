f652decabbfd2343fced66f8f2485d37c0a4c679
rendy <lordkawe1111@gmail.com>
2018-02-23 20:36:44 +0700

    .

--- Inc/aside.php
+++
@@ -1,9 +0,0 @@
-<?php
-<div class="container">
-<div class="header">
- 		<div class="kiri"><p>Pemilihan OSIS</p><p class="periode">Periode 2018-2019</p></div>
- 		<div class="logosatu"></div>
- 		<div class="kanan"><p class="b">Berbasis</p><br><p class="web">Web</p></div>
-	</div>
-</div>
- ?>
